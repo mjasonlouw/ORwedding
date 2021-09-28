c5567af6e17e681ec6d6d075113fbdc18ffa509f
OliviadeKok <olivia.de.kok@gmail.com>
2021-09-28 23:26:16 +0200

    things

--- src/app/default/components/menu/menu.component.ts
+++ src/app/default/components/menu/menu.component.ts
@@ -12,4 +12,46 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openMainMenu() {
+    var menu = document.getElementsByClassName("menu_dropdown_wrapper")[0]; 
+    var menu_items = document.getElementsByClassName("main_menu_item"); 
+  
+    if (menu.classList.contains("closed")) {
+      menu.classList.remove("closed");
+      menu.classList.add("open");
+  
+      for(var i = 0; i<menu_items.length; i++) {
+        menu_items[i].classList.remove("fade_out");
+      }
+  
+      for(var i = 0; i<menu_items.length; i++) {
+        menu_items[i].classList.add("fade_in");
+      }
+  
+      document.getElementsByClassName("open_menu")[0].classList.add("fade_out");
+      document.getElementsByClassName("close_menu")[0].classList.add("fade_in");
+  
+      document.getElementsByClassName("open_menu")[0].classList.remove("fade_in");
+      document.getElementsByClassName("close_menu")[0].classList.remove("fade_out");
+  
+    } else {
+      menu.classList.remove("open");
+      menu.classList.add("closed");
+  
+      for(var i = 0; i<menu_items.length; i++) {
+        menu_items[i].classList.remove("fade_in");
+      }
+  
+      for(var i = 0; i<menu_items.length; i++) {
+        menu_items[i].classList.add("fade_out");
+      }
+  
+      document.getElementsByClassName("open_menu")[0].classList.add("fade_in");
+      document.getElementsByClassName("close_menu")[0].classList.add("fade_out");
+  
+      document.getElementsByClassName("open_menu")[0].classList.remove("fade_out");
+      document.getElementsByClassName("close_menu")[0].classList.remove("fade_in");
+    }
+  }
 }
+
