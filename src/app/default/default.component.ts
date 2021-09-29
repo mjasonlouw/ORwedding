4e79e681677560c64e89c164ee00e89c5f32b93c
Jason <jason.louw@epiuse.com>
2021-09-29 19:47:01 +0200

    things++

--- src/app/default/default.component.ts
+++ src/app/default/default.component.ts
@@ -6,7 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./default.component.scss']
 })
 export class DefaultComponent implements OnInit {
-  
+
   ngOnInit(): void { }
+
+  pageToShow = 'rsvp';
+
+  changePageTo(pageName) {
+    console.log('clicking')
+    console.log(pageName)
+    if (pageName == 'registry') {
+
+    } else {
+      this.pageToShow = pageName;
+
+      this.slideBitch(pageName)
+    }
+  }
+
+  async slideBitch(pageName){
+
+
+    setTimeout(() => {
+      console.log(`.${pageName}Container`)
+
+      const per = document.querySelector(`.${pageName}Container`);
+      per.scrollIntoView({behavior: 'smooth'})
+    }, 10);
+  }
+
+
+
 
 }
